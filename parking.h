57dc6c3d2234c4eecf83d62207ee2ed3379f41a4
Your Name <five90204@gmail.com>
2024-01-04 00:21:38 +0800

    modify parking

--- parking.h
+++ parking.h
@@ -2,8 +2,8 @@
 #include <semaphore.h>
 #include <stdlib.h>
 
-#define FLOORS 12
-#define SPOTS_PER_FLOOR 12
+#define FLOORS 16
+#define SPOTS_PER_FLOOR 16
 #define MAX_TIME 999
 
 sem_t parking_spots[FLOORS][SPOTS_PER_FLOOR];
